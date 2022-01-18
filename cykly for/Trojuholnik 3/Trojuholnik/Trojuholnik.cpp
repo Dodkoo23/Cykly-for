cb10d243cb9d34446b7dcca9ffa21bc72f92f1c0
Dodkoo23 <92848160+Dodkoo23@users.noreply.github.com>
2022-01-18 21:41:10 +0100

    Add files via upload

---
+++ cykly for/Trojuholnik 3/Trojuholnik/Trojuholnik.cpp
@@ -0,0 +1,23 @@
+﻿// Trojuholnik.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	string a = "*";
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j <= (3 - i); j++) {
+			cout << a;
+		}
+
+		cout << endl;
+	}
+	
+}
+
+	
+		
+
---
+++ cykly for/Trojuholník 4/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	
+	string a = "*";
+
+	for (int i = 4; i > 0; i--) {
+		for (int j = 0; j <= (4 - i); j++) {
+			cout << a;
+		}
+
+		cout << endl;
+	}
